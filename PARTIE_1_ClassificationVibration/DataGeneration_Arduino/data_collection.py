c68a49361eb9d7ff1001d838866f0ea70b8d768f
arselanmeghelli <meghelliarselan62@gmail.com>
2025-03-10 21:53:50 +0100

    Add files via upload

---
+++ PARTIE_1_ClassificationVibration/DataGeneration_Arduino/data_collection.py
@@ -0,0 +1,21 @@
+﻿import serial
+import csv
+import time
+
+
+ser = serial.Serial('COM3', 115200)
+time.sleep(2)  
+
+with open("vibrations.csv", "w", newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["X", "Y", "Z", "Label"])  
+
+    print("Collecte en cours... Appuie sur Ctrl+C pour arrêter!! :)")
+    
+    try:
+        while True:
+            line = ser.readline().decode('utf-8').strip()
+            writer.writerow(line.split(","))
+    except KeyboardInterrupt:
+        print("Arrêt de la collecte.")
+        ser.close()
